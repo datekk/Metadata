2637fa35dd7a1cbe554cb1d22e92130a547473e4
Thomas Otto <thomas-otto@gmx.de>
2021-02-06 16:22:51 +0100

    Projektdateien hinzufügen.

---
+++ Metadata/Data/Kunden.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metadata.Data
+{
+    public partial class Kunden
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachnamme { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Metadata/Data/KundenMeta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metadata.Data
+{
+    public class KundenMeta
+    {
+        [Required]
+        public string Vorname { get; set; }
+        [Required]
+        public string Nachname { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage ="Dies ist keine gültige E-Mailadresse")]
+        public string Email { get; set; }
+    }
+}
